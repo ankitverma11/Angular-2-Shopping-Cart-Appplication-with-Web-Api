28a80ab44bbf3110bfb116cb91d7475abc5a9d93
Ankit Verma <anki.verma02@hotmail.com>
2018-11-29 12:19:02 +0530

    add a dropdown on top of the grid to filter the data based on the selected value and call api or all summeries

--- ShoppingCart/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ ShoppingCart/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { error } from 'util';
 
 @Component({
   selector: 'app-fetch-data',
@@ -7,11 +8,24 @@ import { HttpClient } from '@angular/common/http';
 })
 export class FetchDataComponent {
   public forecasts: WeatherForecast[];
+  public cacheForecasts: WeatherForecast[];
+  public summaries: any[];
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
     http.get<WeatherForecast[]>(baseUrl + 'api/SampleData/WeatherForecasts').subscribe(result => {
       this.forecasts = result;
+      this.cacheForecasts = this.forecasts;
     }, error => console.error(error));
+    http.get(baseUrl + 'api/SampleData/GetSummaries').subscribe(x => {
+      this.summaries = x as any[];
+    }, error => console.error(error));
+  }
+
+  filterForeCasts(filterVal: any) {
+    if (filterVal == "0")
+      this.forecasts = this.cacheForecasts;
+    else
+      this.forecasts = this.cacheForecasts.filter((item) => item.summary == filterVal);
   }
 }
 
@@ -20,4 +34,8 @@ interface WeatherForecast {
   temperatureC: number;
   temperatureF: number;
   summary: string;
+}
+
+interface Summary {
+  name: string;
 }
